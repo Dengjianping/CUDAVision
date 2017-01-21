a21d8ce842325c83b05eca9e3571552a625d9686
Dengjianping <djptux@gmail.com>
2017-01-21 14:30:08 +0800

    add a HostTimeRecorder to test how much time runs on host

--- cuVision/timerecorder.h
+++ cuVision/timerecorder.h
@@ -1,38 +1,69 @@
 #include "headers.h"
 
-class TimeRecorder {
+class DeviceTimeRecorder {
 private:
     cudaEvent_t start, end;
 public:
-    TimeRecorder();
+    DeviceTimeRecorder();
     void startRecord();
     void stopRecord();
     float timeCost();
-    ~TimeRecorder();
+    ~DeviceTimeRecorder();
 };
 
-TimeRecorder::TimeRecorder() {
+DeviceTimeRecorder::DeviceTimeRecorder() {
     cudaEventCreate(&start);
     cudaEventCreate(&end);
 }
 
-void TimeRecorder::startRecord() {
+void DeviceTimeRecorder::startRecord() {
     cudaEventRecord(start);
 }
 
-void TimeRecorder::stopRecord() {
+void DeviceTimeRecorder::stopRecord() {
     cudaEventRecord(end);
     cudaEventSynchronize(start);
     cudaEventSynchronize(end);
 }
 
-float TimeRecorder::timeCost() {
+float DeviceTimeRecorder::timeCost() {
     float time;
     cudaEventElapsedTime(&time, start, end);
     return time;
 }
 
-TimeRecorder::~TimeRecorder() {
+DeviceTimeRecorder::~DeviceTimeRecorder() {
     cudaEventDestroy(start);
     cudaEventDestroy(end);
+}
+
+class HostTimeRecorder {
+private:
+    double start, end;
+public:
+    HostTimeRecorder();
+    void startRecord();
+    void stopRecord();
+    double timeCost();
+    ~HostTimeRecorder();
+};
+
+HostTimeRecorder::HostTimeRecorder() {
+    start = end = 0.0;
+}
+
+void HostTimeRecorder::startRecord() {
+    start = (double)cv::getTickCount();
+}
+
+void HostTimeRecorder::stopRecord() {
+    end = (double)cv::getTickCount();
+}
+
+double HostTimeRecorder::timeCost() {
+    return (end - start) / cv::getTickFrequency();
+}
+
+HostTimeRecorder::~HostTimeRecorder() {
+
 }
