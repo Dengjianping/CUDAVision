017c42d9c67e4111c9acfcc4e5bf7911983cffac
Dengjianping <djptux@gmail.com>
2017-02-19 23:39:34 +0800

    update

---
+++ cuVision/utilities.h
@@ -0,0 +1,24 @@
+#include "headers.h"
+
+namespace utility {
+    // show image
+    void showImage(const std::string & title, const cv::Mat & img) {
+        cv::namedWindow(title);
+        cv::imshow(title, img);
+    }
+
+    void callPython() {
+        Py_Initialize();
+
+        PyObject *pyModule = NULL;
+        PyObject *pyFunction = NULL;
+
+        pyModule = PyImport_AddModule("3d");
+        pyFunction = PyObject_GetAttrString(pyModule, "twoDimGaussian");
+
+        PyEval_CallFunction(pyFunction, NULL);
+
+        Py_Finalize();
+        return;
+    }
+}
