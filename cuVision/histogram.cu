77c315c75bdb19278529d0c50a00afa9576ed17d
Dengjianping <djptux@gmail.com>
2017-03-16 23:47:32 +0800

    finished but better normalize the result

--- cuVision/histogram.cu
+++ cuVision/histogram.cu
@@ -5,14 +5,20 @@
 
 #define SIZE 256
 
-__global__ void histogram(uchar *d_input, int in_height, int in_width, size_t in_pitch, uchar *d_output, size_t out_pitch, int *counter, int sum=0) {
+__global__ void histogram(uchar *d_input, int in_height, int in_width, size_t in_pitch, uchar *d_output, int out_height, size_t out_pitch, int *counter, int *d_pixelSum) {
     int row = blockDim.y*blockIdx.y + threadIdx.y;
     int col = blockDim.x*blockIdx.x + threadIdx.x;
 
     if (row < in_height&&col < in_width) {
         uchar *in_pixel = (uchar*)((char*)d_input + row*in_pitch) + col;
         int index = (int)(*in_pixel);
+        if (*d_pixelSum < INT_MAX) {
+            printf("%d\n", *d_pixelSum);
+            atomicAdd(d_pixelSum, 1);
+        }
         counter[index]++;
+        //atomicAdd(d_pixelSum, *in_pixel);
+        //atomicAdd(&counter[index], 1);
     }
     
     //if (row < SIZE&&col < SIZE) {
@@ -20,7 +26,7 @@ __global__ void histogram(uchar *d_input, int in_height, int in_width, size_t in
         //uchar *out_pixel = (uchar*)((char*)d_input + row*out_pitch) + col;
         int hist = counter[col];
         for (size_t i = 0; i < hist; i++) {
-            uchar *out_pixel = (uchar*)((char*)d_output + (299-i)*out_pitch) + col;
+            uchar *out_pixel = (uchar*)((char*)d_output + (out_height - 1 - i)*out_pitch) + col;
             *out_pixel = 0;
         }
     }
@@ -29,18 +35,19 @@ __global__ void histogram(uchar *d_input, int in_height, int in_width, size_t in
 extern "C"
 void cudaHistogram(cv::Mat & input, cv::Mat & output) {
     output = cv::Mat(cv::Size(300, 300), input.type(), cv::Scalar(255));
-
+    
     size_t in_pitch, out_pitch;
     uchar *d_input, *d_output;
     cudaMallocPitch(&d_input, &in_pitch, sizeof(uchar)*input.cols, input.rows);
     cudaMallocPitch(&d_output, &out_pitch, sizeof(uchar)*output.cols, output.rows);
 
     // store counters for each pixel
-    static int temp[256];
-    int *counter;
+    int *counter, *d_pixelSum;
     cudaMalloc(&counter, sizeof(int) * 256);
-    cudaMemcpy(temp, counter, sizeof(int) * 256,cudaMemcpyHostToDevice);
+    cudaMalloc(&d_pixelSum, sizeof(int));
+
     cudaMemset(counter, 0, sizeof(int) * 256);
+    cudaMemset(d_pixelSum, 0, sizeof(int));
 
     const int N = 2;
     cudaStream_t streams[N];
@@ -54,11 +61,14 @@ void cudaHistogram(cv::Mat & input, cv::Mat & output) {
     dim3 blockSize(input.cols / (MAX_THREADS / 2) + 1, input.rows / MAX_THREADS + 1);
     dim3 threadSize(MAX_THREADS / 2, MAX_THREADS);
 
-    histogram <<<blockSize, threadSize>>> (d_input, input.rows, input.rows, in_pitch, d_output, out_pitch, counter);
+    histogram <<<blockSize, threadSize>>> (d_input, input.rows, input.cols, in_pitch, d_output, output.rows, out_pitch, counter, d_pixelSum);
     CUDA_CALL(cudaDeviceSynchronize());
-
+    /*cudaError_t error = cudaDeviceSynchronize();
+    if (error != cudaSuccess) {
+        std::cout << cudaGetErrorString(error) << std::endl;
+    }*/
     cudaMemcpy2D(output.data, output.cols * sizeof(uchar), d_output, out_pitch, output.cols * sizeof(uchar), output.rows, cudaMemcpyDeviceToHost);
 
     for (size_t i = 0; i < N; i++)cudaStreamDestroy(streams[i]);
-    cudaFree(d_input); cudaFree(d_output);
+    cudaFree(d_input); cudaFree(d_output); cudaFree(d_pixelSum);
 }
